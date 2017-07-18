1290f863cea8e6bf74506fcd445009c22956192e
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-07-18 22:33:10 +0300

    Added model files

---
+++ EmployeeManagerService/EmployeeManagerService/Models/Rank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagerService.Models
+{
+    public enum Rank
+    {
+        Developer,
+        Tester,
+        BusinessAnalyst,
+        Manager
+    }
+}
